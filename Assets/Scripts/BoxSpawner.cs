6b6802488f78512609beea68f47482f758450c51
crevedk091 <crevedko91@gmail.com>
2020-10-17 15:20:32 +0300

    init water

---
+++ Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ilhamhe
+{
+    public class BoxSpawner : MonoBehaviour
+    {
+        public GameObject box;
+
+        Vector3 worldPosition;
+
+        private void Update()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = Camera.main.nearClipPlane;
+            worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                GameObject go = Instantiate(
+                    box, new Vector3(
+                        worldPosition.x,
+                        worldPosition.y,
+                        0),
+                    Quaternion.identity) as GameObject;
+                go.transform.localScale = new Vector2(
+                    Random.Range(.5f, 1f),
+                    Random.Range(.5f, 1f));
+                Destroy(go, 3f);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/DynamicWater2D.cs
@@ -0,0 +1,168 @@
+﻿using System.Net.NetworkInformation;
+using System.Security;
+using UnityEngine;
+
+namespace ilhamhe
+{
+    public class DynamicWater2D : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct Bound
+        {
+            public float top;
+            public float right;
+            public float bottom;
+            public float left;
+        }
+
+        [Header("Water Settings")]
+        public Bound bound;
+        public int quality;
+
+        public Material waterMaterial;
+        public GameObject splash;
+
+        private Vector3[] vertices;
+        private Mesh mesh;
+
+        [Header("Phisics Settings")]
+        public float sprigConstant = .02f;
+        public float damping = .1f;
+        public float spread = .1f;
+        public float collisionVelocityFactor = .04f;
+
+        float[] velocities;
+        float[] accelerations;
+        float[] leftDeltas;
+        float[] rightDeltas;
+
+        private float timer;
+
+        private void GenerateMesh()
+        {
+            float range = (bound.right - bound.left) / (quality - 1);
+            vertices = new Vector3[quality * 2];
+
+            for (int i = 0; i < quality; i++)
+            {
+                vertices[i] = new Vector3(bound.left + (i * range), bound.top, 0);
+            }
+
+            for (int i = 0; i < quality; i++)
+            {
+                vertices[i + quality] = new Vector2(bound.left + (i * range), bound.bottom);
+            }
+
+            int[] template = new int[6];
+            template[0] = quality;
+            template[1] = 0;
+            template[2] = quality + 1;
+            template[3] = 0;
+            template[4] = 1;
+            template[5] = quality + 1;
+
+            int marker = 0;
+            int[] tris = new int[((quality - 1) * 2) * 3];
+            for (int i = 0; i < tris.Length; i++)
+            {
+                tris[i] = template[marker++]++;
+                if (marker >= 6) marker = 0;
+            }
+
+
+            MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            if (waterMaterial) meshRenderer.sharedMaterial = waterMaterial;
+
+            MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+            mesh = new Mesh();
+            mesh.vertices = vertices;
+            mesh.triangles = tris;
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+
+            meshFilter.mesh = mesh;
+        }
+
+        private void Start()
+        {
+            InitializePhysics();
+            GenerateMesh();
+            SetBosCollider2D();
+        }
+        
+        void InitializePhysics()
+        {
+            velocities = new float[quality];
+            accelerations = new float[quality];
+            leftDeltas = new float[quality];
+            rightDeltas = new float[quality];
+        }
+
+
+        private void SetBosCollider2D()
+        {
+            BoxCollider2D col = gameObject.AddComponent<BoxCollider2D>();
+            col.isTrigger = true;
+        }
+
+        private void Update()
+        {
+            if (timer <= 0) return;
+            timer -= Time.deltaTime;
+
+            for (int i = 0; i < quality; i++)
+            {
+                float force = sprigConstant * (vertices[i].y - bound.top) + velocities[i] * damping;
+                accelerations[i] = -force;
+                vertices[i].y += velocities[i];
+                velocities[i] += accelerations[i];
+            }
+
+            for (int i = 0; i < quality; i++)
+            {
+                if (i > 0)
+                {
+                    leftDeltas[i] = spread * (vertices[i].y - vertices[i - 1].y);
+                    velocities[i - 1] += leftDeltas[i];
+                }
+                if (i < quality - 1)
+                {
+                    rightDeltas[i] = spread * (vertices[i].y - vertices[i + 1].y);
+                    velocities[i + 1] += rightDeltas[i];
+                }
+            }
+
+            mesh.vertices = vertices;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+            Splash(collision, rb.velocity.y * collisionVelocityFactor);
+        }
+
+        private void Splash(Collider2D collision, float force)
+        {
+            timer = 3f;
+            float radius = collision.bounds.max.x - collision.bounds.min.x;
+            Vector2 center = new Vector2(collision.bounds.center.x, bound.top);
+
+            //GameObject splashGO = Instantiate(splash, new Vector3(center.x, center.y, 0), Quaternion.Euler(0, 0, 60));
+            //Destroy(splashGO, 2f);
+
+            for (int i = 0; i < quality; i++)
+            {
+                if (PointInsideCircle(vertices[i], center, radius))
+                {
+                    velocities[i] = force;
+                }
+            }
+        }
+
+        bool PointInsideCircle(Vector2 point, Vector2 center, float radius)
+        {
+            return Vector2.Distance(point, center) < radius;
+        }
+    }
+}
